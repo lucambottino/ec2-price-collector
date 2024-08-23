76c4f58dc16802b9c322ce02dc187d2db86ba6c6
Ryan Braz Tintore Viana <ryanviana@usp.br>
2024-08-23 15:36:20 -0300

    commit

--- pg-s3.sh
+++ pg-s3.sh
@@ -14,53 +14,60 @@ set -a
 source "$(dirname "$0")/.env"
 set +a
 
-# Get a list of all tables in the database
-TABLES=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -t -c "SELECT tablename FROM pg_tables WHERE schemaname='public';")
+# Define the tables and their respective date columns
+declare -A TABLES
+TABLES=( ["coins_table"]="" ["coin_data_table"]="updated_at" )
 
-# Iterate over each table and export data older than 7 days
-for TABLE in $TABLES; do
-    EXPORT_FILE="/tmp/${TABLE}_old_data_$(date +\%Y\%m\%d).csv"
-    log_message "Exporting data older than 7 days from table $TABLE to $EXPORT_FILE."
+# Iterate over each table
+for TABLE in "${!TABLES[@]}"; do
+    DATE_COLUMN=${TABLES[$TABLE]}
     
-    # Export data older than 7 days to a CSV file
-    PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -c "\copy (SELECT * FROM $TABLE WHERE created_at < NOW() - INTERVAL '7 days') TO '$EXPORT_FILE' WITH CSV HEADER"
-
-    if [ $? -eq 0 ]; then
-        log_message "Data export from table $TABLE successful."
+    if [ -n "$DATE_COLUMN" ]; then
+        EXPORT_FILE="/tmp/${TABLE}_old_data_$(date +\%Y\%m\%d).csv"
+        log_message "Exporting data older than 7 days from table $TABLE using $DATE_COLUMN to $EXPORT_FILE."
+        
+        # Export data older than 7 days to a CSV file
+        PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -c "\copy (SELECT * FROM $TABLE WHERE $DATE_COLUMN < NOW() - INTERVAL '7 days') TO '$EXPORT_FILE' WITH CSV HEADER"
+        
+        if [ $? -eq 0 ]; then
+            log_message "Data export from table $TABLE successful."
+        else
+            log_message "Data export from table $TABLE failed."
+            continue
+        fi
+        
+        # Upload the CSV file to S3
+        log_message "Uploading $EXPORT_FILE to S3."
+        aws s3 cp $EXPORT_FILE s3://$S3_BUCKET/ --region $REGION
+        
+        if [ $? -eq 0 ]; then
+            log_message "Upload of $EXPORT_FILE successful."
+        else
+            log_message "Upload of $EXPORT_FILE failed."
+            continue
+        fi
+        
+        # Delete the CSV file after upload
+        log_message "Deleting the CSV file $EXPORT_FILE."
+        rm $EXPORT_FILE
+        
+        if [ $? -eq 0 ]; then
+            log_message "CSV file $EXPORT_FILE deleted."
+        else
+            log_message "Failed to delete CSV file $EXPORT_FILE."
+        fi
+        
+        # Delete old data from the table
+        log_message "Deleting old data from table $TABLE."
+        PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -c "DELETE FROM $TABLE WHERE $DATE_COLUMN < NOW() - INTERVAL '7 days';"
+        
+        if [ $? -eq 0 ]; then
+            log_message "Old data deletion from table $TABLE successful."
+        else
+            log_message "Old data deletion from table $TABLE failed."
+        fi
     else
-        log_message "Data export from table $TABLE failed."
-        continue
-    fi
-
-    # Upload the CSV file to S3
-    log_message "Uploading $EXPORT_FILE to S3."
-    aws s3 cp $EXPORT_FILE s3://$S3_BUCKET/ --region $REGION
-
-    if [ $? -eq 0 ]; then
-        log_message "Upload of $EXPORT_FILE successful."
-    else
-        log_message "Upload of $EXPORT_FILE failed."
-        continue
-    fi
-
-    # Delete the CSV file after upload
-    log_message "Deleting the CSV file $EXPORT_FILE."
-    rm $EXPORT_FILE
-
-    if [ $? -eq 0 ]; then
-        log_message "CSV file $EXPORT_FILE deleted."
-    else
-        log_message "Failed to delete CSV file $EXPORT_FILE."
-    fi
-
-    # Delete old data from the table
-    log_message "Deleting old data from table $TABLE."
-    PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -c "DELETE FROM $TABLE WHERE created_at < NOW() - INTERVAL '7 days';"
-
-    if [ $? -eq 0 ]; then
-        log_message "Old data deletion from table $TABLE successful."
-    else
-        log_message "Old data deletion from table $TABLE failed."
+        log_message "Skipping table $TABLE because it doesn't have a date column."
     fi
 done
 
